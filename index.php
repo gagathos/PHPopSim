55cc9fb6638191ca896a34203d4c7da3a4c2f907
root <gagathos@thecommotion.com>
2015-07-09 22:41:08 -0400

    Reduced run time because of exponential population growth running out of memory :-)

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 /* Variables */
 
-$years = 6000; //how many years do you want the simulation to run for?
+$years = 20; //how many years do you want the simulation to run for?
 
 
 require_once("Organism.php");
@@ -37,6 +37,7 @@ print_stat("Final Year", $humans->year);
 print_stat("Count Alive", count($humans->organisms));
 print_stat("Count Dead", count($humans->cemetary));
 print_stat("Oldest Age", $humans->getOldest()->getAge($humans)); //This should be the same as years until we implement other ways for organisms to die
+print_stat("Fertilest Fertility", $humans->getHighestGeneEffect('fertility'));
 print_stat("Max Generation", $humans->getLatestGeneration());
 
 //print_organism($humans->getOldest());
